bcff8b5e7bbd6849fe94b62fd421e232ef598482
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-09-14 15:57:00 +0200

    build(WebAPI): Add composite indexes for PlexMovie and PlexTvShowEpisodes EF Migration

---
+++ src/Data/Migrations/20260914132324_AddTorznabFeedOrderingIndexes.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reaparr.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTorznabFeedOrderingIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PlexTvShowEpisodes_AddedAt_PlexServerId_PlexApiRatingKey",
+                table: "PlexTvShowEpisodes",
+                columns: new[] { "AddedAt", "PlexServerId", "PlexApiRatingKey" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlexMovie_AddedAt_PlexServerId_PlexApiRatingKey",
+                table: "PlexMovie",
+                columns: new[] { "AddedAt", "PlexServerId", "PlexApiRatingKey" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PlexTvShowEpisodes_AddedAt_PlexServerId_PlexApiRatingKey",
+                table: "PlexTvShowEpisodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PlexMovie_AddedAt_PlexServerId_PlexApiRatingKey",
+                table: "PlexMovie");
+        }
+    }
+}
