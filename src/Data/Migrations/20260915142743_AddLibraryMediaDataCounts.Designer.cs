fe87121a8746b1f3dfe75efea9e58b90fb2e9208
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-09-15 16:29:09 +0200

    build(WebAPI): Add media data counts for movies and episodes in Plex library EF migration

--- src/Data/Migrations/20260915142743_AddLibraryMediaDataCounts.Designer.cs
+++ src/Data/Migrations/20260915142743_AddLibraryMediaDataCounts.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reaparr.Data;
 
@@ -10,9 +11,11 @@ using Reaparr.Data;
 namespace Reaparr.Data.Migrations
 {
     [DbContext(typeof(ReaparrDbContext))]
-    partial class ReaparrDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260915142743_AddLibraryMediaDataCounts")]
+    partial class AddLibraryMediaDataCounts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Data/Migrations/20260915142743_AddLibraryMediaDataCounts.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reaparr.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLibraryMediaDataCounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TvShowCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 16)
+                .OldAnnotation("Relational:ColumnOrder", 15);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SeasonCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 17)
+                .OldAnnotation("Relational:ColumnOrder", 16);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Outdated",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 23)
+                .OldAnnotation("Relational:ColumnOrder", 21);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsEnabled",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER",
+                oldDefaultValue: true)
+                .Annotation("Relational:ColumnOrder", 24)
+                .OldAnnotation("Relational:ColumnOrder", 22);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenresCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 21)
+                .OldAnnotation("Relational:ColumnOrder", 19);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EpisodeCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 18)
+                .OldAnnotation("Relational:ColumnOrder", 17);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CountriesCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 22)
+                .OldAnnotation("Relational:ColumnOrder", 20);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ActorsCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 20)
+                .OldAnnotation("Relational:ColumnOrder", 18);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EpisodeMediaDataCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Relational:ColumnOrder", 19);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MovieMediaDataCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Relational:ColumnOrder", 15);     
+            
+            migrationBuilder.Sql(
+                """
+                UPDATE "PlexLibraries"
+                SET "MovieMediaDataCount" =
+                    CASE
+                        WHEN "Type" = 'Movie' THEN (
+                            SELECT COUNT(*)
+                            FROM "PlexMovieData"
+                            WHERE "PlexMovieData"."PlexLibraryId" = "PlexLibraries"."Id"
+                        )
+                        ELSE 0
+                    END;
+                """
+            );
+            migrationBuilder.Sql(
+                """
+                UPDATE "PlexLibraries"
+                SET "EpisodeMediaDataCount" =
+                    CASE
+                        WHEN "Type" = 'TvShow' THEN (
+                            SELECT COUNT(*)
+                            FROM "PlexTvShowEpisodeData"
+                            WHERE "PlexTvShowEpisodeData"."PlexLibraryId" = "PlexLibraries"."Id"
+                        )
+                        ELSE 0
+                    END;
+                """
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EpisodeMediaDataCount",
+                table: "PlexLibraries");
+
+            migrationBuilder.DropColumn(
+                name: "MovieMediaDataCount",
+                table: "PlexLibraries");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TvShowCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 15)
+                .OldAnnotation("Relational:ColumnOrder", 16);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SeasonCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 16)
+                .OldAnnotation("Relational:ColumnOrder", 17);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Outdated",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 21)
+                .OldAnnotation("Relational:ColumnOrder", 23);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsEnabled",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER",
+                oldDefaultValue: true)
+                .Annotation("Relational:ColumnOrder", 22)
+                .OldAnnotation("Relational:ColumnOrder", 24);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenresCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 19)
+                .OldAnnotation("Relational:ColumnOrder", 21);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EpisodeCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 17)
+                .OldAnnotation("Relational:ColumnOrder", 18);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CountriesCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 20)
+                .OldAnnotation("Relational:ColumnOrder", 22);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ActorsCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 18)
+                .OldAnnotation("Relational:ColumnOrder", 20);
+        }
+    }
+}
